9881ae9382cf568619a909cf2a09494b3e787ddd
Konstantin Kurganov <kurganovk@gmail.com>
2020-09-14 20:18:10 +0300

    Refactor detectChanges function

--- app/database/detectChanges.ts
+++ app/database/detectChanges.ts
@@ -1,66 +1,61 @@
 import { Store, withId, Changes, DatabaseChanges } from '../types';
-import { measureTime } from '../utils/utils';
+// import { measureTime } from '../utils/utils';
 import { pick, pickBy, mapObjIndexed, compose } from 'ramda'; 
 
-
-
-let toObjById = (list:withId[]) => {
-    let tmp = list.reduce(
-        (obj,item) => {  
-            obj[item._id]=item; 
-            return obj; 
-        },
-        {}
-    );
-    return tmp;
-}
-
-
+// let toObjById = (list:withId[]) => {
+//     let tmp = list.reduce(
+//         (obj,item) => {  
+//             obj[item._id]=item; 
+//             return obj; 
+//         },
+//         {}
+//     );
+//     return tmp;
+// }
 
 export let detectChanges : (state:Store) => (newState:Store) => Changes =
     state =>
     compose( 
         mapObjIndexed(
-            (val:any[], key:string) : DatabaseChanges<any> => {
+            (value:any[], key:string) : DatabaseChanges<any> => {
                 let changes = { add:[], remove:[], update:[] };
                 let prev = state[key];                
-                let next = val; 
+                let next = value; 
 
-                if(prev.length===next.length){     //items updated
-
-                    let obj = measureTime(toObjById, 'toObjById updated')(prev);
+                if (prev.length===next.length) {     //items updated
+                    // let obj = measureTime(toObjById, 'toObjById updated')(prev);
 
                     next.forEach( 
                         item => { 
-                            if(item!==obj[item._id]){
+                            let foundItem = prev.find((val) => val._id === item._id);
+                            if (item !== foundItem) {
                                 changes.update.push(item);
                             }
                         } 
                     )                    
 
-                }else if(prev.length<next.length){ //items added
-
-                    let obj = measureTime(toObjById, 'toObjById added')(prev);
+                } else if (prev.length<next.length) { //items added
+                    // let obj = measureTime(toObjById, 'toObjById added')(prev);
 
                     next.forEach( 
                         item => { 
-                            if(!obj[item._id]){
+                            let foundItem = prev.find((val) => val._id === item._id);
+                            if (!foundItem) {
                                 changes.add.push(item);
-                            }else if(item!==obj[item._id]){
+                            } else if (item !== foundItem) {
                                 changes.update.push(item);
                             }
                         } 
                     )
 
-                }else if(prev.length>next.length){ //items removed
-
-                    let obj = measureTime(toObjById, 'toObjById removed')(next);
+                } else if(prev.length>next.length) { //items removed
+                    // let obj = measureTime(toObjById, 'toObjById removed')(next);
 
                     prev.forEach( 
                         item => { 
-                            let foundItem = next.find((val) => {val._id === item._id});
+                            let foundItem = next.find((val) => val._id === item._id);
                             if (!foundItem) {
-                                 //item does not exist in new state
+                                //item does not exist in new state
                                 changes.remove.push(item);
                             } else if (item !== foundItem) { 
                                 changes.update.push(foundItem);
